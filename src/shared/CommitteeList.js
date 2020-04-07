71565b700ee80ba868c8d29ea822eb968e54cf67
Moneexa <14besemsyed@seecs.edu.pk>
2020-04-07 19:02:29 +0500

    Add files via upload

---
+++ src/shared/CommitteeList.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import './CommitteeList.css'
+import {faUserFriends} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+class CommitteeList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            commitee: {
+                Name: "", start_date: "", end_date: "", saving_amount: ""
+            },
+            committee_arr: []
+        }
+
+    }
+    handleClick = () => {
+        document.getElementById('form1').style.display = "block"
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const newList = this.state.committee_arr.concat(this.state.commitee)
+        this.setState({
+            committee_arr: newList
+        })
+        console.log(this.state.commitee)
+    }
+    handleNameOnChange = (e) => {
+        var _name = e.target.value;
+        this.setState({
+            commitee: {
+                Name: _name, start_date: this.state.commitee.start_date, 
+                end_date:this.state.commitee.end_date, saving_amount:this.state.commitee.saving_amount
+            }
+        })
+    }
+    handleStartDateOnChange = (e) => {
+        var _date = e.target.value;
+        this.setState({
+            commitee: {
+                Name: this.state.commitee.start_date, start_date: _date,
+                end_date:this.state.commitee.end_date,saving_amount:this.state.commitee.saving_amount
+            }
+        })
+    }
+    handleEndDateOnChange = (e) => {
+        var _enddate = e.target.value;
+        this.setState({
+            commitee: {
+                Name: this.state.commitee.start_date, end_date: _enddate,
+                start_date:this.state.commitee.start_date,saving_amount:this.state.commitee.saving_amount
+            }
+        })
+    }
+    handleSavingAmountOnChange = (e) => {
+        var _amount = e.target.value;
+        this.setState({
+            commitee: {
+                Name: this.state.commitee.start_date, start_date: this.state.commitee.start_date,
+                end_date:this.state.commitee.end_date, saving_amount:_amount
+            }
+        })
+    }
+    render() {
+        return (<div className="CommitteeList_comp">
+
+            <div className="flex-direction-column">
+                {this.state.committee_arr.map((values, index) => {
+                    return (<div  key={index} style={{backgroundColor:"#dedbdb"}}>
+                        <div id="prefix-icon">
+                      <FontAwesomeIcon className="prefix-icon"  icon={faUserFriends}/>
+                      </div>
+                        <h4>{values.Name}</h4>
+                <p>{values.start_date}</p><span>-</span><p>{values.end_date}</p>
+                    </div>)
+                })}
+            </div>
+            <form id="form1" onSubmit={this.handleSubmit} style={{ display: "none" }} >
+                <input type="text" placeholder="Name" value={this.state.Name} onChange={this.handleNameOnChange} />
+                <input className="datepicker" data-date-format="mm/dd/yyyy" value={this.state.start_date} onChange={this.handleStartDateOnChange} />
+                <input className="datepicker" data-date-format="mm/dd/yyyy" value={this.state.end_date} onChange={this.handleEndDateOnChange} />
+                <input type="text" placeholder="Saving Amount" value={this.state.saving_amount} onChange={this.handleSavingAmountOnChange} />
+
+                <button id="add-bg" tabIndex="0" onClick={this.handleSubmit} className="ep--enhanced">
+                    <div id="edit-bg-icon"></div>
+                    <span id="edit-bg-text">Save</span>
+                </button>
+            </form>
+            <div id="edit-bg" tabIndex="0" role="button" onClick={this.handleClick} className="ep-enhanced" aria-label="Customize this page" title="Customize this page">
+                <div id="edit-bg-icon"></div>
+                <span id="edit-bg-text">+ Create</span>
+            </div>
+
+
+        </div>)
+    }
+}
+export default CommitteeList
--- src/shared/header.js
+++ src/shared/header.js
@@ -1,1 +1,36 @@
+import React, { Component } from 'react'
+import './header.css'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
+class header extends Component {
+
+    render() {
+        return (
+            <div className="header_comp">
+                <Navbar expand="md sm lg">
+                    
+                        <Nav className="parent_div">
+                            <Nav.Link>
+
+                                <h5>Committee Register</h5>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <div className="prefix_icon">
+                                    <FontAwesomeIcon className="prefix-icon" icon={faSearch} />
+                                </div>
+                            </Nav.Link>
+
+                        </Nav>
+                </Navbar>
+
+
+
+            </div>
+        )
+    }
+
+}
+export default header;
