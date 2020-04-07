2cf543a6eb4fb3503d018140f4f02f1cd6b3b8fb
Moneexa <14besemsyed@seecs.edu.pk>
2020-04-07 23:47:48 +0500

    Add files via upload

--- src/shared/CommitteeList.js
+++ src/shared/CommitteeList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './CommitteeList.css'
 import { faUserFriends } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {Modal, Button} from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 class CommitteeList extends Component {
     constructor(props) {
@@ -32,6 +32,8 @@ class CommitteeList extends Component {
         this.setState({
             committee_arr: newList
         })
+        this.setState({ showSheet: false })
+
         console.log(this.state.commitee)
     }
     handleNameOnChange = (e) => {
@@ -79,28 +81,38 @@ class CommitteeList extends Component {
                         <div id="prefix-icon">
                             <FontAwesomeIcon className="prefix-icon" icon={faUserFriends} />
                         </div>
-                        <h4>{values.Name}</h4><br />
+                        <h4>{values.Name}</h4><br /><br />
                         <p>{values.start_date}</p><span>-</span><p>{values.end_date}</p>
-                    </div>)
+                    </div>
+                    )
                 })}
             </div>
 
-            <div id="edit-bg" tabIndex="0" role="button" onClick={() => {this.setState({showSheet: true})}} className="ep-enhanced" aria-label="Customize this page" title="Customize this page">
+            <div id="edit-bg" tabIndex="0" role="button" onClick={() => { this.setState({ showSheet: true }) }} className="ep-enhanced" aria-label="Customize this page" title="Customize this page">
                 <div id="edit-bg-icon"></div>
                 <span id="edit-bg-text">+ Create</span>
             </div>
 
-            <Modal show={this.state.showSheet} onHide={() => {this.setState({showSheet: false})}}>
-                <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal show={this.state.showSheet} onHide={() => { this.setState({ showSheet: false }) }}>
+
+                <Modal.Body>
+                    <form id="form1" onSubmit={this.handleSubmit} >
+                        <input type="text" value={this.state.Name} onChange={this.handleNameOnChange} placeholder="Name" />
+                        <input type="text" value={this.state.start_date} onChange={this.handleStartDateOnChange} placeholder="Start Date" />
+                        <input type="text" value={this.state.end_date} onChange={this.handleEndDateOnChange} placeholder="End Date" />
+                        <input type="text" value={this.state.saving_amount} onChange={this.handleSavingAmountOnChange} placeholder="Saving Amount" />
+
+
+                    </form>
+
+
+                </Modal.Body>
                 <Modal.Footer>
-                <Button variant="secondary" onClick={() => {this.setState({showSheet: false})}}>
-                    Close
+                    <Button variant="secondary" onClick={() => { this.setState({ showSheet: false }) }}>
+                        Close
                 </Button>
-                <Button variant="primary" onClick={() => {this.setState({showSheet: false})}}>
-                    Save Changes
+                    <Button variant="primary" onClick={this.handleSubmit}>
+                        Save Changes
                 </Button>
                 </Modal.Footer>
             </Modal>
