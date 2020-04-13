2c01c34c2fcbf134d28e86d8dc234236a27d5658
Moneexa <14besemsyed@seecs.edu.pk>
2020-04-13 23:12:21 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './shared/header'
 import CommitteeList from './shared/CommitteeList'
+import {CommitteeAttendance} from './shared/CommitteeAttendance'
+import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-       <CommitteeList /> 
+
+      <CommitteeList>
+        <Router>
+
+          <Link to="/committeeAtt">
+            <h4 component={Link} to="/committeeList" className="m-0 Comitteee_name">CommitteeName</h4>
+          </Link>
+          <Switch>
+
+            <Route exact path="/committeeAtt">
+              <CommitteeAttendance />
+            </Route>
+          </Switch>
+        </Router>
+      </CommitteeList>
     </div>
   );
 }
