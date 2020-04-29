d5e16c224e692eeba862656a8fb1e972eea23153
Moneexa <14besemsyed@seecs.edu.pk>
2020-04-29 19:55:18 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import Header from './shared/header'
 import CommitteeList from './shared/CommitteeList'
 import { CommitteeAttendance } from './shared/CommitteeAttendance'
 import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./shared/store/store"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <CommitteeList />
-          </Route>
-          <Route exact path="/committeeAtt">
-            <CommitteeAttendance />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <CommitteeList />
+            </Route>
+            <Route exact path="/committeeAtt">
+              <CommitteeAttendance />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
